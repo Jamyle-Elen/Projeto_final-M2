bd046602e17f2d34551288a7c94a6988aa10895e
Jamyle Elen <jamyleelen09@gmail.com>
2024-04-04 16:45:48 -0300

    scroll reveal

---
+++ page/articles/articles.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', function() {
+    var element = document.querySelector('.scroll-element');
+    var position = element.getBoundingClientRect();
+
+    if (position.top < window.innerHeight && position.bottom >= 0) {
+        element.classList.add('visible');
+    } else {
+        element.classList.remove('visible');
+    }
+
+    var articles = document.querySelectorAll('.article');
+    articles.forEach(function(article) {
+        var articlePosition = article.getBoundingClientRect();
+
+        if (articlePosition.top < window.innerHeight && articlePosition.bottom >= 0) {
+            article.classList.add('visible');
+        } else {
+            article.classList.remove('visible');
+        }
+    });
+});

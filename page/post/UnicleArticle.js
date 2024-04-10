6351c262465e2f0bb136c78b0be914f18b43d792
Jamyle Elen <jamyleelen09@gmail.com>
2024-04-10 19:53:05 -0300

    page articles

--- page/post/UnicleArticle.js
+++ page/post/UnicleArticle.js
@@ -1,3 +1,40 @@
+import { articles } from "../../data/database.js";
+
+let id = location.search.replace('?id=', '')
+console.log(id);
+
+let index = articles.findIndex(function (article) {
+  return article.id == id;
+});
+console.log(index)
+if (index == -1) {
+  window.location.href = "../../page/articles/articles.html"
+}else {
+  let articleFound = articles[index];
+console.log(articleFound);
+
+const imgOne = document.getElementById('img1');
+const imgTwo = document.getElementById('img2');
+const imgThree = document.getElementById('img3');
+const dadContent = document.getElementById('content');
+const titleArticle = document.getElementById('title');
+
+titleArticle.innerText = articleFound.title;
+imgOne.src = articleFound.image.one;
+imgTwo.src = articleFound.image.two;
+imgThree.src = articleFound.image.three;
+
+dadContent.innerHTML = articleFound.contentHTML;
+
+}
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     var slideIndex = 0;
     var slides = document.querySelectorAll('.carousel-slide');
@@ -37,4 +74,6 @@ document.addEventListener("DOMContentLoaded", function() {
     prevBtn.addEventListener('click', prevSlide);
     nextBtn.addEventListener('click', nextSlide);
   });
+  
+
   
